c22e4a27ac66047e468bdc8194bc98c989d59942
balexdalex42 <atatoian@ucsd.edu>
2025-04-30 05:35:12 -0700

    commented and added more conditions for sumValues()

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -6,7 +6,7 @@
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
-    if(typeof num1 !== typeof num2)
+    if(typeof num1 !== typeof num2 || typeof num1 !== typeof 5) //make sure that num1 and num2 have same type
         return false;
     if (add) {
         let result = 0;
